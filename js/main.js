48b3cdd3c9b06b9874bf56a2dfc47c25c59b8b04
mbaskhairoun <mbaskhairoun@gmail.com>
2022-02-20 04:13:34 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,25 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const keys = document.querySelectorAll(".keyboard-row button");
 
   function getNewWord() {
-    fetch(
-      `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-          "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((res) => {
-        word = res.word;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    word="hello";
   }
 
   function getCurrentWordArr() {
